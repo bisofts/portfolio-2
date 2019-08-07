50550273da9e85d9644adac801101ea86905dc64
Mike S <mikesonnenberg1@gmail.com>
2019-08-07 15:07:52 -0700

    renamed and rearranged index.html files

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 Vue.component('vue-nav', {
-  template: '<nav><input type="checkbox" id="nav" class="hidden" /><label for= "nav" class= "nav-open" > <i></i> <i></i> <i></i></label><div class="nav-container"><ul><li><a href="/design/index.html">DESIGN</a></li><li><a href="/ux/index.html">UX</a></li><li><a href="/contact/index.html">CONTACT</a></li></ul></div></nav>'
+  template: '<nav><input type="checkbox" id="nav" class="hidden" /><label for= "nav" class= "nav-open" > <i></i> <i></i> <i></i></label><div class="nav-container"><ul><li><a href="/design.html">DESIGN</a></li><li><a href="/ux.html">UX</a></li><li><a href="/contact.html">CONTACT</a></li></ul></div></nav>'
 });
-// <img src="img/ms-logo-lines.min.svg" alt="Mike Sonnenberg Logo, Home" class="navlogo">
-//  <nav>
-//   <input type="checkbox" id="nav" class="hidden" />
-//   <label for="nav" class="nav-open"><i></i><i></i><i></i></label>
-//   <div class="nav-container">
-//     <ul>
-//       <li><a href="/design/index.html">DESIGN</a></li>
-//       <li><a href="/ux/index.html">UX</a></li>
-//       <li><a href="/contact/index.html">CONTACT</a></li>
-//     </ul>
-//   </div>
-// </nav>
+/* <img src="img/ms-logo-lines.min.svg" alt="Mike Sonnenberg Logo, Home" class="navlogo">
+ <nav>
+  <input type="checkbox" id="nav" class="hidden" />
+  <label for="nav" class="nav-open"><i></i><i></i><i></i></label>
+  <div class="nav-container">
+    <ul>
+      <li><a href="/design/index.html">DESIGN</a></li>
+      <li><a href="/ux/index.html">UX</a></li>
+      <li><a href="/contact/index.html">CONTACT</a></li>
+    </ul>
+  </div>
+</nav> */
 Vue.component('vue-logo', {
   template: '<div id="nav-logo"> <img src="/img/ms-logo-lines.svg" alt="Mike Sonnenberg Logo"> </div>'
 });
